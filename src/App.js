c155fbfa1402c64928b4c1e27fab03292a73a200
Shubhangi <shubhangi.108om@gmail.com>
2024-02-11 01:31:22 +0530

    Add CartItem Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import CartIteam from './CartItem';
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      <CartIteam/>
     </div>
   );
 }
+
 
 export default App;
